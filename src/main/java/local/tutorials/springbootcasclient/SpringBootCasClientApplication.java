7cd3ae74089ce924dbdaf14210a35e104fd33a76
jn <jn@kali.kali>
2022-05-05 23:12:02 -0400

    Added init code from Spring Boot for the Spring Boot CAS client.

---
+++ src/main/java/local/tutorials/springbootcasclient/SpringBootCasClientApplication.java
@@ -0,0 +1,13 @@
+package local.tutorials.springbootcasclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCasClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCasClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/tutorials/springbootcasclient/SpringBootCasClientApplicationTests.java
@@ -0,0 +1,13 @@
+package local.tutorials.springbootcasclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCasClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
